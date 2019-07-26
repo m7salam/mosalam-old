eaca9e134f835c41b77d02826dfbb362249910f1
m7salam <mabdelsalam911@gmail.com>
2019-07-26 13:38:21 +0800

    editing prod.py

--- src/mosalam/settings/prod.py
+++ src/mosalam/settings/prod.py
@@ -4,7 +4,7 @@ from mosalam.settings.base import *
 DEBUG = False
 ALLOWED_HOSTS = ['mosalam.herokuapp.com',]
 
-
+WSGI_APPLICATION = 'wsgi.application'
 
 try:
     from mosalam.settings.local import *
