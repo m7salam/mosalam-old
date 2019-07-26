3bb2fd6784f13b661af113d8b50fba6b395e99b2
m7salam <mabdelsalam911@gmail.com>
2019-07-26 14:25:55 +0800

    returning the wsgi settings

--- src/mosalam/settings/prod.py
+++ src/mosalam/settings/prod.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = ['mosalam.herokuapp.com',]
 
-WSGI_APPLICATION = 'mosalam.wsgi'
+WSGI_APPLICATION = 'mosalam.wsgi.application'
 
 try:
     from .local import *
--- src/mosalam/wsgi.py
+++ src/mosalam/wsgi.py
@@ -8,11 +8,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mosalam.settings.prod')
-sys.path.append('/src/mosalam/settings')
 
 application = get_wsgi_application()
