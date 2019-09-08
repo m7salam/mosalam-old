b5cf71cc106df78f8fc56b02dc1b057eb4aed2c0
m7salam <mabdelsalam911@gmail.com>
2019-09-08 21:30:21 +0800

    finished most of the blocks in the new style

--- src/projects/models.py
+++ src/projects/models.py
@@ -14,18 +14,26 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+
+class Images(models.Model):
+    image = models.ImageField(blank=True, null=True, upload_to='Projects/')
+
+    def __str__(self):
+        return self.image.url
+
 class Project(models.Model):
 
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
     description = models.TextField()
-    image = models.ImageField(blank=True, null=True, upload_to='Projects/')
+    image = models.ManyToManyField('Images', blank=True, null=True, related_name='projects')
     video = models.TextField(blank=True, null=True)
     technology = models.ManyToManyField('Technology', related_name='projects')
     category = models.ManyToManyField('Category', related_name='projects')
     website = models.CharField(max_length = 255, blank=True, null=True)
     ajax_id = models.CharField(max_length=255, default="general")
     featured = models.BooleanField(default=False, null=True, blank=True)
+    thumbnail = models.ImageField(blank=True, null=True, upload_to='Thumbnail/')
 
     def __str__(self):
         return self.title
@@ -34,9 +42,3 @@ class Project(models.Model):
         return reverse('project', kwargs={'slug': self.slug})
 
 
-class Images(models.Model):
-    project = models.ForeignKey("Project", on_delete=models.CASCADE)
-    image = models.ImageField(blank=True, null=True, upload_to = 'Projects/')
-
-    def __str__(self):
-        return self.project.title + "Images"
